d4f94d7089f5df0b646fd1bd60dd5bf21372b7ab
tase4ka112 <99361625+tase4ka112@users.noreply.github.com>
2024-05-29 06:46:08 +0000

    Deploying to gh-pages from @ tase4ka112/practice@7c6e2c4947764513589747e0cc41047679b31071 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-7fccb1c1.js";import{S as i,E as e,N as t,P as a}from"./assets/vendor-83fd77d1.js";new i(".swiper",{modules:[e,t,a],effect:"flip",loop:!0,grabCursor:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=commonHelpers2.js.map
